9c78f3ea6970b79255ed1017cb2e55c06f4ea6c7
DlonMusk <knight777@hotmail.ca>
2022-05-30 21:42:18 -0400

    made UI mobile friendly with bootstrap

--- js/script.js
+++ js/script.js
@@ -9,51 +9,21 @@ let time = moment().format("L");
 // grab all elements needed
 
 //search elements
-
+let citySearchEl = $('.city-search');
+let searchBtnEl = $('.city-search-btn');
 
 // current weather elements
 let cityDateEl = $(".city-date");
 let currentTempEl = $(".current-temp");
 let currentWindEl = $(".current-wind");
 let currentHumidityEl = $(".current-humidity");
-let uvBtnEl = $('.uv-btn');
+let currentCityWeatherEl = $('.current-city-weather');
 
 // weather forecast elements
 let forecastCardsEl = $('.forecast-cards');
 
 
-// initial fetch for coordinates and initial weather data
-fetch(url).then(function (response) {
-    return response.json();
-}).then(function (data) {
-    console.log(data);
-    // set current weather
-    cityDateEl.text(`${data.name} (${time})`);
-    currentTempEl.text(`Temp: ${data.main.temp} Deg C`);
-    currentWindEl.text(`Wind: ${data.wind.speed}KPH`);
-    currentHumidityEl.text(`Humidity: ${data.main.humidity}%`);
 
-
-    // set up variables for new api call
-    let lat = data.coord.lat;
-    let lon = data.coord.lon;
-    console.log(lat + " " + lon);
-    let oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIkey}&units=metric`
-
-
-    // second fetch for UV and forecast
-    fetch(oneCallUrl).then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        console.log(data);
-        uvBtnEl.text(data.current.uvi);
-        console.log(data.daily[1]);
-        // create and populate the forecast cards
-        for(let i = 0; i < 5; i++){
-            createCardFunction(data.daily[i], moment().add(i, 'd').format('L'))
-        }
-    })
-})
 
 let createCardFunction = (dayData, day) => {
     // create all divs for cards
@@ -61,35 +31,81 @@ let createCardFunction = (dayData, day) => {
     let cardBody = $('<div>').addClass('card-body');
     cardBody.append($('<h5>').addClass('card-date').text(day));
     cardBody.append($('<img>').addClass('img-fluid').attr('src', `http://openweathermap.org/img/wn/${dayData.weather[0].icon}@2x.png`));
-    cardBody.append($('<p>').addClass('card-temp').text(`Temp: ${dayData.temp.day}`));
+    cardBody.append($('<p>').addClass('card-temp').text(`Temp: ${dayData.temp.day}°C`));
     cardBody.append($('<p>').addClass('card-wind').text(`Wind: ${dayData.wind_speed}`));
     cardBody.append($('<p>').addClass('card-humidity').text(`Humidity: ${dayData.humidity}`));
     card.append(cardBody);
-    
-    console.log(card);
+
+
     forecastCardsEl.append(card);
 }
 
+let getWeatherFor = (event) => {
+    
+    event.preventDefault();
+    city = citySearchEl.val();
+    if(city == ""){
+        // popup
+        return;
+    }
+    
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=metric`;
 
-// set default city to london ontario
-// fetch(url).then(function(response){
-//     return response.json();
-// }).then(function(data){
-//     console.log(data);
-//     // populate page
-//     cityDateEl.text(`${data.name} (${time})`);
-//     currentTempEl.text(`Temp: ${data.main.temp}`);
-//     currentWindEl.text(`Wind: ${data.wind.speed}KPH`);
-//     currentHumidityEl.text(`Humidity: ${data.main.humidity}%`);
-//     currentUvEl.text(`UV Index: `);
+    // initial fetch for coordinates and initial weather data
+    fetch(url).then(function (response) {
+        if(response.status > 200){
+            document.location = "./index.html";
+        }
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+        forecastCardsEl.empty();
+        $('.current-uv').remove();
 
-// })
+        // set current weather
+        cityDateEl.text(`${data.name} (${time})`);
+        currentTempEl.text(`Temp: ${data.main.temp}°C`);
+        currentWindEl.text(`Wind: ${data.wind.speed}KPH`);
+        currentHumidityEl.text(`Humidity: ${data.main.humidity}%`);
+
+        // create button of city searched for ul
+        
 
 
-// let searchFunction = () => {
+        // set up variables for new api call
+        let lat = data.coord.lat;
+        let lon = data.coord.lon;
 
-// }
+        let oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIkey}&units=metric`
 
-// let searchBtnEl = $('.city-search-btn');
-// searchBtnEl.on('click', searchFunction);
-// console.log(searchBtnEl);
+
+        // second fetch for UV and forecast
+        fetch(oneCallUrl).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            // create uv element and button
+            console.log(data);
+            let currentUvEl = $('<p>').addClass('current-uv').text("UV Index: ");
+            let uvBtnEl = $('<button>').addClass('uv-btn').text(data.current.uvi);
+            currentUvEl.append(uvBtnEl);
+            currentCityWeatherEl.append(currentUvEl);
+            
+            // clear forecast-cards div then create and populate new forecast cards
+            
+            for (let i = 0; i < 5; i++) {
+                createCardFunction(data.daily[i], moment().add(i, 'd').format('L'))
+            }
+        })
+    })
+
+    console.log(citySearchEl.val())
+}
+
+
+
+searchBtnEl.on('click', getWeatherFor);
+
+
+
+
+
